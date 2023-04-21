2e6a75188c9d7a6e507293cd121c181151aedd51
Swetha Sighakolli <112651667+ssighakoli@users.noreply.github.com>
2023-04-21 01:38:17 -0500

    Update settings.py

--- HawkSoar/settings.py
+++ HawkSoar/settings.py
@@ -135,7 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+//STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'db_connect' / 'static']
 ASGI_APPLICATION = "HawkSoar.asgi.application"
 
